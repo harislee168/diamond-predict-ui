db714b114fb26b58a40cff627e19e87ca13d7076
Haris Lee <haris.lee168@gmail.com>
2023-08-28 14:01:42 +0800

    added bootstrap css and created the header

---
+++ src/layout/navbar/Navbar.js
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <div className='container-fluid py-3 bg-primary d-flex justify-content-center align-items-center'>
+      <h3 className='text-white'>Diamond Valuation</h3>
+    </div>
+  )
+}
+
+export default Navbar
